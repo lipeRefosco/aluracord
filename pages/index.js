685404985012e21b3be86b30d71c3d30fd38390d
Felipe Refosco <luisfelipe.refosco@hotmail.com>
2022-01-26 14:01:47 -0300

    Primeiro commit do projeto

---
+++ pages/index.js
@@ -0,0 +1,149 @@
+import { Box, Button, Text, TextField, Image } from "@skynexui/components"
+import React from "react";
+import { useRouter } from "next/router";
+import appConfig from '../config.json'; 
+
+
+function Title(props){
+    const Message = props.children;
+    const Tag = props.tag;
+    
+    return <>
+      <Tag>{Message}</Tag>
+      
+      <style jsx>{`
+        ${Tag} {
+          color: ${appConfig.theme.colors.primary[500]};
+        }
+      `}</style>
+    </>
+}
+
+function HomePage() {
+    const [username, setUsername] = React.useState('lipeRefosco');
+    const [imgUsername, setImgUsername] = React.useState(username)
+    const router = useRouter();
+
+    return (
+        <>
+        <Box
+            styleSheet={{
+            display: 'flex', alignItems: 'center', justifyContent: 'center',
+            backgroundColor: appConfig.theme.colors.neutrals[300],
+            backgroundImage: 'url(https://wallpapercave.com/wp/ATujZnh.jpg)',
+            backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            }}
+        >
+        <Box
+          styleSheet={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            flexDirection: {
+              xs: 'column',
+              sm: 'row',
+            },
+            width: '100%', maxWidth: '700px',
+            borderRadius: '5px', padding: '32px', margin: '16px',
+            boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+            backgroundColor: appConfig.theme.colors.neutrals[700],
+          }}
+        >
+          {/* Formulário */}
+          <Box 
+            as="form"
+            onSubmit={ eventInfo => {
+              eventInfo.preventDefault();
+              router.push("/chat");
+            }}
+            styleSheet={{
+              display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
+              width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
+            }}
+          >
+            
+            <Title tag="h2">Boas vindas de volta!</Title>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+              {appConfig.name}
+            </Text>
+
+            <TextField
+              value={username}
+              onChange={event => {
+                // Recupera o value do input
+                const valorCampo = event.target.value;
+
+                if(valorCampo.length >= 2){
+                  setImgUsername(valorCampo)
+                }
+                // Reseta a variavel com o valor do campo
+                setUsername(valorCampo);
+              }}
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  backgroundColor: appConfig.theme.colors.neutrals[800],
+                },
+              }}
+            />
+            <Button
+              type='submit'
+              label='Entrar'
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals[999],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              maxWidth: '200px',
+              padding: '16px',
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: '1px solid',
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: '10px',
+              flex: 1,
+              minHeight: '240px',
+            }}
+          >
+            <Image
+              styleSheet={{
+                borderRadius: '50%',
+                marginBottom: '16px',
+              }}
+              src={`https://github.com/${imgUsername}.png`}
+            />
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: '3px 10px',
+                borderRadius: '1000px'
+              }}
+            >
+              {username}
+            </Text>
+          </Box>
+          {/* Photo Area */}
+        </Box>
+      </Box>
+    </>
+    );
+  }
+  
+  export default HomePage
