3947ad1bed992effd0cbb8649604f20e4471e181
Felipe Refosco <luisfelipe.refosco@hotmail.com>
2022-01-29 13:27:46 -0300

    Printar e postar as mensagens prontas

--- pages/chat.js
+++ pages/chat.js
@@ -1,87 +1,127 @@
 import { Box, Text, TextField, Image, Button } from "@skynexui/components";
 import React from "react";
 import appConfig from "../config.json"
+import {createClient} from "@supabase/supabase-js"
+
+const URL_BANCO = "https://ewizjfkkpmecgevxkmto.supabase.co";
+const URL_KEY_BANCO = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM5OTY1OSwiZXhwIjoxOTU4OTc1NjU5fQ.ADsl5qkgSd7A4eQ2PHGx6ZotWuQlN5kDPvIzhq-f4Z8";
+
+const supabaseCliente = createClient(URL_BANCO, URL_KEY_BANCO);
 
 export default function ChatPage(){
 
     const [logedUserWrite, setLogedUserWrite] = React.useState('');
     const [chat, setChat] = React.useState([]);
 
+
+    refreshChat();
+
     return (
         <>
             <Box
                 styleSheet={{
-                    
-                    display: 'flex', alignItems: 'center', justifyContent: 'center',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
                     color: 'white',
                     backgroundColor: appConfig.theme.colors.neutrals[300],
                     backgroundImage: 'url(https://wallpapercave.com/wp/ATujZnh.jpg)',
-                    backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundSize: 'cover',
+                    backgroundBlendMode: 'multiply',
             }}> {/* Box com o fundo */}
 
                 <Box styleSheet={{
-                    maxWidth: '90%', maxHeight: '95vh',
-                    display: 'flex', flexDirection: 'column', flex: '1', 
+                    maxWidth: '90%',
+                    maxHeight: '95vh',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: '1', 
                     backgroundColor: appConfig.theme.colors.neutrals[500],
-                    padding: '3%', borderRadius: '5px',
+                    padding: '3%',
+                    borderRadius: '5px',
                     outline: `1px solid ${appConfig.theme.colors.neutrals[800]}`,
                 }}> {/* Container all chat */}
                     
 
                     <Header/> {/* Header da página com o botão de logout */}
 
-                    <Box tag="ul" styleSheet={{
-                        overflow: 'visible',
-                        display: 'flex', flexFlow:'column-reverse',
-                        alignItems: 'flex-end',gap:'16px',
+                    <Box styleSheet={{
+                        backgroundColor: 'rgba(0,0,0,0.4)',
+                        padding: '1em',
                         borderRadius: '5px',
-                        overflow: 'scroll',
-                        // outline: `1px solid ${appConfig.theme.colors.neutrals[999]}`,
+                        outline: `1px solid ${appConfig.theme.colors.neutrals[999]}`,
                     }}> {/* Container messages */}
                         
                         <AllMessages messages={chat} />
 
-                    </Box> {/* End container messages */}
-
-                    <TextField value={logedUserWrite} type="textarea"
-                        placeholder="Digite sua mensagem"
+                        <TextField value={logedUserWrite}
+                            type="textarea"
+                            placeholder="Digite sua mensagem"
                             styleSheet={{
-                                backgroundColor: appConfig.theme.colors.neutrals[800],
-                                borderColor: appConfig.theme.colors.neutrals[999],
-                                color: 'white',
-                        }}
-                        onChange={props => {
-                            const dataInserted = props.target.value
-                            setLogedUserWrite(dataInserted);
-                        }}
-                        onKeyPress={props => {
-                            const key = props.key;
-                            if(key === 'Enter'){
-                                props.preventDefault();
+                                    marginTop: '10px',
+                                    backgroundColor: appConfig.theme.colors.neutrals[500],
+                                    borderColor: appConfig.theme.colors.neutrals[700],
+                                    color: 'white',
+                            }}
+                            onChange={props => {
+                                const dataInserted = props.target.value
+                                setLogedUserWrite(dataInserted);
+                            }}
+                            onKeyPress={props => {
+                                const key = props.key;
+                                if(key === 'Enter'){
+                                    props.preventDefault();
 
-                                // Cadastra na lista de mensagem
-                                setChat([
-                                    {
-                                        id: chat.length + 1,
-                                        from: 'lipeRefosco',
-                                        message: logedUserWrite
-                                    },
-                                    ...chat
-                                ]);
+                                    // Cadastra na lista de mensagem
+                                    // setChat([
+                                    //     {
+                                    //         from: 'lipeRefosco',
+                                    //         message: logedUserWrite
+                                    //     },
+                                    //     ...chat
+                                    // ]);
 
-                                // Limpa o campo de texto
-                                setLogedUserWrite('');
-                                console.log('Mensagem cadastrada!');
-                            }
-                            console.log(chat)
-                        }}
-                    ></TextField>
+                                    sendMessage();
+
+                                    // refreshChat();
+
+                                    // Limpa o campo de texto
+                                    setLogedUserWrite('');
+                                }
+                            }}
+                        ></TextField>
+                    </Box> {/* End container messages */}
 
                 </Box>{/* End container all chat */}
 
             </Box>
         </>
     )
+
+    function refreshChat(){
+        React.useEffect(() =>{
+            supabaseCliente
+            .from("mensagens")
+            .select("*")
+            .order("id", { ascending: false })
+            .then( ({data}) => {
+                console.log(data)
+                setChat(data)
+            });
+        }, [logedUserWrite]);
+    }
+
+    function sendMessage(){
+        supabaseCliente.from("mensagens")
+                       .insert([
+                           {from: "lipeRefosco", message: logedUserWrite}
+                       ])
+                       .then(() => {
+                            console.log('Mensagem cadastrada!');
+                       })
+    }
+
 }
 
 function Header(){
@@ -105,35 +145,57 @@ function Header(){
 function AllMessages(props){
     return(
         <>
-            {props.messages.map( (message) => {
-                return <Message key={message.id} from={message.from} message={message.message} />
-            })}
-            
+            <Box tag="ul" styleSheet={{ display: 'flex', flexDirection: 'column-reverse', gap: '0.8em'}}>
+                {props.messages.map( (message) => {
+                    return <Message styleSheet={{
+                        width: '100%'
+                    }} key={message.id} date={message.created_at} from={message.from} message={message.message} />
+                })}
+            </Box>
         </>
     )
 }
 
 function Message(props){
     const from = props.from;
-    const message = props.message;
-    const srcMessage = `https://github.com/${from}.png`
-    
+    const fromDate = props.date;
+    const fromDateFormated = fromDate.slice(0,10);
+    const fromMessage = props.message;
+    const fromAvatar = `https://github.com/${from}.png`
+
     return (
         <>
             <li>
-                <Image src={srcMessage} alt={from}
-                    styleSheet={{ width: '25px', borderRadius: '50%', marginRight: '5px'}}
-                />
-                <Text styleSheet={{marginRight: '5px'}}>{from}:</Text>
-                <Text>{message}</Text>
+                
+                <Text styleSheet={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    <Image src={fromAvatar} alt={from}
+                        styleSheet={{
+                            height: '20px',
+                            borderRadius: '50%',
+                            marginRight: '5px',
+                            display: 'inline'}}
+                    />
+                        {from}:
+                    <Text styleSheet={{
+                        fontSize: '0.7em',
+                        color: appConfig.theme.colors.neutrals[100],
+                        backgroundColor: appConfig.theme.colors.neutrals[800],
+                        padding: '3px',
+                        margin: '3px',
+                        borderRadius: '5px'
+                    }}>{fromDateFormated}</Text>
+                </Text>
+                
+                <Text styleSheet={{
+                    display: 'flex',
+                    alignItems: 'center'
+                }}>
+                    {fromMessage}
+                </Text>
             </li>
-            <style jsx>{`
-                li {
-                    width: 100%;
-                    display: flex;
-                    align-items: center;
-                }
-            `}</style>
         </>
     )
 }
